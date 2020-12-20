6caf1c37723ff1e04cc06bfa87d61bb1792692da
HillKTON <56476391+hillkton@users.noreply.github.com>
2020-12-20 19:11:12 +0500

    Добавление многопоточности

--- RandomPic/Form1.Designer.cs
+++ RandomPic/Form1.Designer.cs
@@ -31,15 +31,17 @@ namespace RandomPic
         {
             this.button1 = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button1.Location = new System.Drawing.Point(91, 360);
+            this.button1.Location = new System.Drawing.Point(86, 415);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(275, 78);
+            this.button1.Size = new System.Drawing.Size(275, 48);
             this.button1.TabIndex = 0;
             this.button1.Text = "Какая-то херня";
             this.button1.UseVisualStyleBackColor = true;
@@ -47,25 +49,46 @@ namespace RandomPic
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(22, 28);
+            this.pictureBox1.Location = new System.Drawing.Point(17, 50);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(422, 291);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
             // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(17, 363);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(422, 36);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(427, 38);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Успех к провалу: 0 | 0";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(456, 450);
+            this.ClientSize = new System.Drawing.Size(456, 475);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.button1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Рандом Картинки";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -73,6 +96,8 @@ namespace RandomPic
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- RandomPic/Form1.cs
+++ RandomPic/Form1.cs
@@ -8,23 +8,69 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Leaf.xNet;
+using System.Threading;
 
 namespace RandomPic
 {
     public partial class Form1 : Form
     {
+        int[] stats = new int[2] { 0, 0 };
+        string temp_url;
+        bool proc = true;
+        Image picture;
         static string url()
         {
             Random rand = new Random();
             string output = "";
             string seed = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM0123456789";
+            string[] file_format = { "png", "jpg" };
             int len = 7;
             for (int i = 0; i < len; i++)
             {
                 output += seed[rand.Next(seed.Length)];
             }
-            return $"https://i.imgur.com/{output}.png";
+            return $"https://i.imgur.com/{output}.{file_format[rand.Next(2)]}";
+            //return "https://i.imgur.com/FttQoPk.jpg";
         }
+
+        private void SearchPicture()
+        {
+            HttpRequest request = new HttpRequest();
+            var ui = new Action(() => UpdateGUI());
+            while (proc)
+            {
+                temp_url = url();
+                HttpResponse resp = request.Get(temp_url);
+                if (resp.Address.AbsoluteUri != "https://i.imgur.com/removed.png")
+                {
+                    picture = Image.FromStream(resp.ToMemoryStream());
+                    stats[0] += 1;
+                    proc = false;
+                }
+                else
+                {
+                    stats[1] += 1;
+                }
+                this.Invoke(ui);
+            }
+        }
+
+        private void UpdateGUI()
+        {
+            if (proc)
+            {
+                label1.Text = $"Успех к провалу: {stats[0]} | {stats[1]}";
+            } 
+            else
+            {
+                label1.Text = $"Успех к провалу: {stats[0]} | {stats[1]}";
+                pictureBox1.Image = picture;
+                textBox1.Text = temp_url;
+                button1.Enabled = true;
+                textBox1.Enabled = true;
+            }
+        }
+
         public Form1()
         {
             InitializeComponent();
@@ -32,23 +78,13 @@ namespace RandomPic
 
         private void button1_Click(object sender, EventArgs e)
         {
-            bool proc = true;
-            HttpRequest request = new HttpRequest();
-            do
+            button1.Enabled = false;
+            textBox1.Enabled = false;
+            proc = true;
+            for (int i = 0; i < 11; i++)
             {
-                string temp_url = url();
-                HttpResponse resp = request.Get(temp_url);
-                if (resp.Address.AbsoluteUri != "https://i.imgur.com/removed.png")
-                {
-                    pictureBox1.Image = Image.FromStream(resp.ToMemoryStream());
-                    proc = false;
-                } else
-                {
-                    MessageBox.Show(temp_url);
-                }
-            } 
-            while (proc);
-            
+                new Thread(new ThreadStart(SearchPicture)).Start();
+            }
         }
     }
 }
